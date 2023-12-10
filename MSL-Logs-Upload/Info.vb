9bd28cd88945cf96f0667f13083f035c18118815
luluxiaoyu <ljy.xiaoyu@qq.com>
2023-12-10 20:07:55 +0800

    添加项目文件。

---
+++ MSL-Logs-Upload/Info.vb
@@ -0,0 +1,47 @@
+﻿Module Info
+    Public Sub Loginfo(ByVal str As String)
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.Write("[")
+        Console.ForegroundColor = ConsoleColor.Blue
+        Console.Write("信息")
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.Write("]")
+        Console.ForegroundColor = ConsoleColor.White
+        Console.WriteLine(str)
+        Console.ResetColor() ' 重置为默认颜色
+    End Sub
+    Public Sub Logerr(ByVal str As String)
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.Write("[")
+        Console.ForegroundColor = ConsoleColor.Red
+        Console.Write("错误")
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.Write("]")
+        Console.ForegroundColor = ConsoleColor.White
+        Console.WriteLine(str)
+        Console.ResetColor() ' 重置为默认颜色
+    End Sub
+    Public Sub Login(ByVal str As String)
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.Write("[")
+        Console.ForegroundColor = ConsoleColor.Yellow
+        Console.Write("需要用户输入")
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.Write("]")
+        Console.ForegroundColor = ConsoleColor.White
+        Console.WriteLine(str)
+        Console.ResetColor() ' 重置为默认颜色
+    End Sub
+
+    Public Sub Logcho(ByVal str As String, num As String)
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.Write("（")
+        Console.ForegroundColor = ConsoleColor.White
+        Console.Write(num)
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.Write("）")
+        Console.ForegroundColor = ConsoleColor.White
+        Console.WriteLine(str)
+        Console.ResetColor() ' 重置为默认颜色
+    End Sub
+End Module
---
+++ MSL-Logs-Upload/Module1.vb
@@ -0,0 +1,150 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Reflection
+Imports System.Text
+Imports System.Text.RegularExpressions
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+
+Module Module1
+    Sub Main()
+
+        Console.OutputEncoding = Encoding.UTF8
+        Console.WriteLine("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣶⣶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⠿⠛⠋⠉⠩⣄⠘⢿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⡏⠑⠒⠀⠀⣀⣀⠀⠀⢹⠈⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣷⡀⢀⣰⣿⡿⣿⣧⠀⠀⢡⣾⣧⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣯⣴⣿⠿⣄⣤⣾⡿⠟⠛⠛⠿⢿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣶⠿⠛⠋⠙⣿⣏⠀⠀⢻⣿⣡⣀⣀⠀⠀⠀⠀⢹⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⠿⠋⠁⠀⣀⣤⣶⣾⣿⣿⣤⣤⣾⣿⠉⠉⠙⠻⣿⠆⢀⣾⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡿⠋⠁⠀⣀⣴⣿⠿⠛⠉⠀⢀⣿⡿⠿⠟⢿⣆⠀⢀⣴⣯⣴⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠋⠀⠀⣠⣾⠟⠋⠀⠀⠀⠀⠀⣈⣿⣷⣤⣴⣾⣿⣈⣻⣿⡟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣰⡿⠁⠀⣠⡾⠋⠁⠀⠀⢀⣠⣴⠶⠞⠛⠛⠋⠉⠉⠉⠉⠙⠛⠻⠷⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠰⣿⠁⠀⠀⣿⣄⣀⣠⣴⡾⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⣿⠿⣶⣄⠀⠀⠀⢀⣠⡄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠛⠶⠶⢾⣿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⣄⠉⠙⠻⠿⠟⢹⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠟⠁⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⠀⠀⠀⠀⠘⣿⣿⣦⣀⠀⠲⣾⣁⠀⠀⠀⠀⠀⡀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣦⡀⠀⠀⠀⣿⡿⣿⣿⣿⡆⠀⠉⠛⠛⠛⠛⢻⡏⠀
+⠀⠀⠀⠀⠀⠀⠀⣠⡾⠁⠀⠀⠀⠀⠀⠀⠀⠀⢀⡄⣸⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⠛⢦⡀⠰⣿⣿⣿⣽⣿⡇⠀⠀⠀⠀⠀⢠⡿⠀⠀
+⠀⠀⠀⠀⣀⣤⡾⢻⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⡏⠙⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠰⣽⣶⣄⠉⠻⣿⣿⣧⠀⠀⢀⣤⣾⠟⠁⠀⠀
+⢰⣶⡾⠛⠋⠉⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⢸⣸⡇⠀⣨⣿⣾⡋⠀⠀⠀⠀⢀⠀⠀⣿⡀⠀⠈⠛⢷⣄⠈⠛⣿⡆⠀⠘⣿⡀⠀⠀⠀⠀
+⠀⠙⠿⣦⣀⠀⠀⠀⠀⠀⠀⡾⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠋⠀⠙⢿⣦⣀⠀⠀⠘⣷⣄⣹⣧⠀⠀⠀⠈⢻⣦⠀⠈⠋⠀⠀⠘⣧⠀⠀⠀⠀
+⠀⠀⠀⠈⠛⠿⢶⡶⠃⠀⣰⠃⠀⠀⠀⠀⠀⢠⣿⠃⠀⠀⠀⠀⠀⠀⠉⠻⢷⣦⣤⣘⣿⡛⠛⠀⢀⣴⣶⣦⡹⣷⡀⠀⠀⠀⠀⠸⣧⠀⠀⠀
+⠀⠀⠀⠀⠀⢠⡿⠃⠀⢀⡟⠀⠀⠀⠀⠀⠀⣼⣿⠀⠀⢀⣴⣿⣿⣷⡄⠀⠀⠈⠉⠉⠉⠉⠀⠀⢸⣿⣿⣿⣷⠻⣧⠀⠀⠀⠀⠀⢿⡆⠀⠀
+⠀⠀⠀⠀⢰⣿⠁⠀⠀⢸⠁⠀⠀⠀⠀⠀⠈⠋⣿⠀⠀⠸⣿⣿⣿⣿⡷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠿⠟⠃⠀⢻⣧⠀⠀⠀⠀⠸⣧⠀⠀
+⠀⠀⠀⠀⣿⡇⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠉⠻⠿⠋⠀⠀⢠⡀⠀⠀⣀⣀⣀⣸⠇⠀⠀⠀⠀⠀⠈⢿⣧⠀⠀⠀⠀⣿⡀⠀
+⠀⠀⠀⢰⣿⠁⠀⠀⢰⡏⠀⠀⠀⠀⠀⠀⠀⠀⢿⡀⠀⠀⠀⠀⠀⣸⠀⠀⠈⠛⠒⠛⠉⠈⠉⠀⠀⠀⠀⠀⠀⠀⠀⢸⡟⠀⠀⠀⠀⢸⡇⠀
+⠀⠀⠀⢸⣿⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠚⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⠁⠀⠀⠀⠀⢸⣿⠀
+⠀⠀⠀⢸⣿⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣤⣶⡿⠋⣿⠀⠀⠀⠀⠀⠀⣿⠀
+⠀⠀⠀⠘⣿⡄⠀⠀⢸⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣧⣄⣀⣀⣀⣠⣤⣶⣶⣾⣿⣿⣿⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⣠⠀⠀⢠⣿⡄
+⠀⠀⠀⠀⢻⣧⠀⠀⠸⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣟⠛⢻⡟⢻⡉⠉⣫⣀⠀⠀⠀⠉⠉⠛⠙⠿⣷⣿⡿⠁⢀⣰⡟⠀⠀⢸⣿⠀
+⠀⠀⠀⠀⠈⢻⣧⡀⠀⠹⣷⡀⠀⠀⠀⠀⢠⠀⠀⠀⠘⣿⣿⣿⢻⡿⠀⠈⠷⠟⠁⠘⢾⣿⣶⣤⣶⣾⡇⠀⣿⣿⣤⣶⣿⣿⠃⠀⠀⣾⡟⠀
+⠀⠀⠀⠀⠀⠈⠻⣷⣄⡀⠹⣷⣄⠀⠀⠀⢸⣷⣤⡀⠀⠈⢻⣿⣯⣤⠀⠀⣠⡀⠀⢀⣼⣿⣿⣿⣿⣟⠁⠐⠿⣿⣿⣿⣿⠋⠀⢀⣾⠟⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠙⠿⣷⣮⣽⣷⣶⣤⣤⣿⣿⣿⣷⣶⣦⣭⣿⣿⣧⣠⠵⢯⡆⠚⣯⢿⠋⠛⠛⢫⣀⣠⣾⣿⢿⣿⣥⣤⠶⠛⠁⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⢁⣾⣿⣿⣿⠿⠿⠿⠿⠻⢿⣿⣿⣷⣦⣤⣤⣀⣤⣤⣄⣶⣿⣿⡿⠟⠉⠀⠀⢻⡄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⡇⠀⠀⣀⡀⠈⢿⣧⠀⠉⠙⠛⠛⠛⠛⠛⠛⠉⠁⠀⠀⠀⠀⠀⠀⢿⡄⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣴⣿⣿⡏⠛⠉⢻⣿⣿⣿⣾⣿⣿⠀⢸⣿⠀⠀⠀⠀⠀⠙⠓⠢⠀⠀⠀⠀⠀⠸⠀⠀⠀⠘⣿⡄⠀⠀⠀⠀⠀"
+)
+        Dim assembly As Assembly = assembly.GetExecutingAssembly()
+        Dim version As Version = assembly.GetName().Version
+
+        Loginfo("欢迎使用MSL-Server日志上传工具！ ")
+        Loginfo("Version: " & version.ToString() & " by luluxiaoyu")
+        Dim filePath As String = Path.Combine(Directory.GetCurrentDirectory(), "MSL", "ServerList.json")
+        If File.Exists(filePath) Then
+            Try
+                Loginfo("准备读取MSL服务器列表配置···")
+                Loginfo("以下是在MSL中读取到的服务器列表")
+                '——————json read——————
+                ' JSON文件路径
+                Dim jsonFilePath As String = Path.Combine(Directory.GetCurrentDirectory(), "MSL", "ServerList.json")
+                ' 读取JSON文件内容
+                Dim jsonText As String = System.IO.File.ReadAllText(jsonFilePath)
+                ' 解析JSON数据
+                Dim jsonObject As JObject = JsonConvert.DeserializeObject(Of JObject)(jsonText)
+                Dim i = 0
+                ' 遍历JSON对象
+                For Each property1 As JProperty In jsonObject.Properties()
+                    ' 获取"name"属性的值
+                    Dim name As String = property1.Value("name").ToString()
+                    ' 输出"name"的值
+                    Logcho(name, i)
+                    i = i + 1
+                Next
+
+                ' 创建一个正则表达式对象
+                Dim regex As New Regex("^[0-" & i - 1 & "]$")
+                Dim servertag
+                While True
+                    Login("请选择需要导出日志的服务器（输入括号内的整数）：")
+                    Dim input As String = Console.ReadLine()
+
+                    ' 使用正则表达式检查用户输入的数据
+                    If regex.IsMatch(input) Then
+                        servertag = input
+                        Exit While
+                    Else
+                        Logerr("您选择的服务器不存在，请重新选择！")
+                    End If
+                End While
+                Loginfo（"准备为你导出：" & jsonObject(servertag)("name").ToString() & "中的日志")
+                '读取日志
+
+                If File.Exists(Path.Combine(jsonObject(servertag)("base").ToString(), "logs", "latest.log")) Then
+                    ' 读取本地文件latest.log的内容
+                    Dim logFilePath As String = Path.Combine(jsonObject(servertag)("base").ToString(), "logs", "latest.log")
+                    Dim logContent As String = File.ReadAllText(logFilePath)
+
+                    ' 创建一个HttpWebRequest对象
+                    Dim request As HttpWebRequest = CType(WebRequest.Create("https://api.mclo.gs/1/log"), HttpWebRequest)
+                    request.Method = "POST"
+                    request.ContentType = "application/x-www-form-urlencoded"
+
+                    ' 将logContent转换为字节数组
+                    Dim postData As String = "content=" & Uri.EscapeDataString(logContent)
+                    Dim byteArray As Byte() = Encoding.UTF8.GetBytes(postData)
+
+                    ' 设置ContentLength属性
+                    request.ContentLength = byteArray.Length
+
+                    ' 获取请求流，并将logContent写入请求流
+                    Using dataStream As Stream = request.GetRequestStream()
+                        dataStream.Write(byteArray, 0, byteArray.Length)
+                    End Using
+
+                    ' 获取响应
+                    Dim response As WebResponse = request.GetResponse()
+
+                    ' 从响应中读取数据
+                    Dim responseContent As String
+                    Using dataStream As Stream = response.GetResponseStream()
+                        Using reader As New StreamReader(dataStream)
+                            responseContent = reader.ReadToEnd()
+                        End Using
+                    End Using
+
+                    ' 解析JSON数据
+                    Dim jsonObject2 As JObject = JObject.Parse(responseContent)
+                    If jsonObject2("success").ToString() = True Then
+                        Loginfo("日志导出成功：" & jsonObject2("url").ToString())
+                        My.Computer.Clipboard.SetText("大家好，我在开启MC服务器时遇到了问题，想请教一下！" & vbCrLf & "这是我的日志：" & jsonObject2("url").ToString())
+                        Loginfo("已经为你把链接复制到剪贴板，你可以粘贴去群里求助了！")
+                    Else
+                        Logerr("日志导出失败！原因：" & jsonObject2("error").ToString())
+                    End If
+
+                Else
+                    Logerr("没有检测到服务器日志！")
+                End If
+            Catch ex As Exception
+                Logerr("程序运行时出错：" & ex.Message)
+            End Try
+
+        Else
+            Logerr("MSL服务器列表配置读取失败！")
+            Logerr("请确保本软件放在MSL同一目录下！")
+        End If
+        Loginfo("程序执行完成！按任意键退出······")
+        Console.ReadKey(True)
+    End Sub
+
+End Module
